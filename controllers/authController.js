80da823e5e465d4cc94fa3f37ced5b058fc55891
ikeppu <andrejs.sahniks@gmail.com>
2021-10-03 19:13:38 +0300

    cookie controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,20 +12,18 @@ const signToken = (id) => {
   });
 };
 
-const createSendToken = (user, statusCode, res) => {
+const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
+
+  res.cookie('jwt', token, {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
-    httpOnly: false,
-    secure: true,
-    domain: 'localhost',
-    sameSite: 'none',
-    path: '/',
-  };
-  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+    httpOnly: true,
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  });
+
+  // Remove password from output
   user.password = undefined;
 
-  res.cookie('jwt', token, cookieOptions);
   res.status(statusCode).json({
     status: 'success',
     token,
